b855461b3eb23c29bbeee399157f895343906b21
xloouis <8938475+xloouis@users.noreply.github.com>
2025-02-06 20:37:20 +0800

    Quartz sync: Feb 6, 2025, 8:37 PM

--- content/index.md
+++ content/index.md
@@ -1,5 +1,5 @@
 ---
-title: Welcome to Quartz
+title: Welcome
 ---
 
 This is a blank Quartz installation.
--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "xloouis.github.io/garden",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
